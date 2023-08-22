2f772c838d779603b57391c49c6c1cfa87436041
Omar Hafez <omarhafez@Omars-MacBook-Pro-2.local>
2023-08-22 18:53:22 -0400

    fixed create deck

--- src/Layout/DeckCreate.js
+++ src/Layout/DeckCreate.js
@@ -1,58 +1,71 @@
 import React, { useState } from "react";
-import { Link, useHistory } from "react-router-dom";
-import DeckForm from "./DeckForm"
-import {createDeck} from "../utils/api/index"
+import "../App.css";
+import { Link } from "react-router-dom";
+import { createDeck } from "../utils/api";
+import { useHistory } from "react-router-dom";
+
+function DeckCreate() {
+  const [name, setName] = useState("");
+  const [description, setDescription] = useState("");
+  const handleNameChange = (event) => setName(event.target.value);
+  const handleDescriptionChange = (event) => setDescription(event.target.value);
+
+  const history = useHistory();
+
+  const handleSubmit = async (event) => {
+
+    event.preventDefault();
+    const abortController = new AbortController();
+    const deck = {};
+    deck.name = name;
+    deck.description = description;
+    await createDeck(deck, abortController.signal)
+
+    setName("");
+    setDescription("");
+    history.push("/");
+  }
 
 
-function DeckCreate(){
-    const initialFormState = {
-        name: "",
-        description: "",
-      };
-      const [formData, setFormData] = useState({ ...initialFormState });
-      const handleChange = ({ target }) => {
-        const value = target.value;
-       setFormData({
-       ...formData,
-       [target.name]: value,
-     });
-    };
-    const history = useHistory();
-    const handleSubmit = (event) => {
-        let output = [];
-        event.preventDefault();
-        //console.log("Submitted:", formData);
-        async function updateData() {
-           try {
-            const output = await createDeck(formData)
-            history.push(`/decks/${output.id}`);
-          } catch (error) {
-            if (error.name === "AbortError") {
-              // Ignore `AbortError`
-              console.log("Aborted");
-          } else {
-              throw error;
-          }
-        }
-      }
-      updateData();   
-      };
-    return (
-        <div>
-            <nav aria-label="breadcrumb">
-                <ol className="breadcrumb">
-                    <li className="breadcrumb-item" key="0"><Link to="/">Home</Link></li>
-                    <li className="breadcrumb-item active" aria-current="page" key="1">Create Deck</li>
-                </ol>
-            </nav>
-            <br />
-            <h2>Create Deck</h2>
-            <form onSubmit={handleSubmit}>
-                <DeckForm formData={formData} handleChange={handleChange} />
-                <Link to="/" className="btn btn-secondary">Cancel</Link> &nbsp;
-                <button type="submit" className="btn btn-primary">Save</button>
-            </form>
+  return (
+    <>
+      <div className="main-container">
+        <nav className="breadcrumb-nav">
+          <ul className="breadcrumb-list">
+            <Link to="/">
+              <li className="breadcrumbx-item">
+                Home
+                <span> / </span>
+              </li>
+            </Link>
+            <li className="breadcrumb-item">Create Deck</li>
+          </ul>
+        </nav>
+
+        <div className="card-name">
+          <h1>Create Deck</h1>
         </div>
-    );
+
+        <form onSubmit={handleSubmit}>
+          <div className="card-number">
+            <h2>Name</h2>
+            <input type="text" id="deckname" name="deckname" placeholder="Deck Name" onChange={handleNameChange} value={name} />
+            <h2>Description</h2>
+            <textarea id="description" name="deckdescription" rows="4" cols="50" placeholder="Brief description of the deck" onChange={handleDescriptionChange} value={description}></textarea>
+          </div>
+
+          <div className="deck-buttons-container">
+            <div className="btn-group-left">
+              <button className="button-sm btn-cancel" onClick={() => history.push(`/`)}>Cancel</button>
+              <button className="button-sm btn-submit" id="submit" type="submit">Submit</button>
+
+            </div>
+          </div>
+
+        </form>
+      </div>
+    </>
+  );
 }
+
 export default DeckCreate;
